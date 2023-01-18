bd10a66cc99c2da10f4e87e08b27901346945d34
Nathan Beam <nathan.w.beam@gmail.com>
2023-01-17 20:49:30 -0800

    Deprecating a field

--- src/schema.js
+++ src/schema.js
@@ -1,4 +1,4 @@
-const { gql } = require('apollo-server');
+const { gql } = require("apollo-server");
 
 const typeDefs = gql`
   type Query {
@@ -35,8 +35,10 @@ const typeDefs = gql`
     author: Author!
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
-    "The track's approximate length to complete, in minutes"
-    length: Int
+    "The track's approximate length to complete, in seconds"
+    length: Int @deprecated(reason: "use durationInSeconds")
+    "The track's approximate length to complete, in seconds"
+    durationInSeconds: Int
     "The number of modules this track contains"
     modulesCount: Int
     "The track's complete description, can be in markdown format"
@@ -67,6 +69,7 @@ const typeDefs = gql`
     content: String
     "The module's video url, for video-based modules"
     videoUrl: String
+    durationInSeconds: Int
   }
 `;
 
